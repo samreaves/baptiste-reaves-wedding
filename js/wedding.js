0eb8262d2ff9fa429709c85790c2645eb822d3aa
Sam Reaves <thesamreaves@gmail.com>
2014-01-29 22:17:40 -0500

    Themed navbar with jQuery workaround on collapse and css for whole navbar

---
+++ js/wedding.js
@@ -0,0 +1,13 @@
+/* 
+ Baptiste-Reaves Wedding
+ Written by Sam Reaves & Brittany Baptiste
+ January 29th, 2014
+ */
+
+$(document).ready(function() {
+    $(".navbar-toggle").hover(function() {
+        $(this).children().toggleClass("icon-bar-workaround");
+    });
+});
+
+
