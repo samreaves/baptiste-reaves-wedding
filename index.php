03d2e9dbf017621e00667f78a1abb33881f81b80
Sam Reaves <thesamreaves@gmail.com>
2014-02-11 21:34:39 -0500

    Changed maps. Fixed registry picture

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
         <link href="./css/style.css" rel="stylesheet">
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        < !--[if lt IE 9] >
+        <script src = "https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" ></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
         <style>
             .col-md-9 {
@@ -128,8 +128,10 @@
             <h2 class="center header">Directions</h2>
             <div class="row">
                 <div class="col-xs-12">
-                    <h4>Our Venue</h4>
-                    <center><iframe src="https://mapsengine.google.com/map/embed?mid=zVnRrsBSuH_c.kz6hePyLmMwc" width="480" height="480"></iframe></center>
+                    <h4 class="center">Our Venue</h4>
+                    <!-- <center><iframe src="https://mapsengine.google.com/map/embed?mid=zVnRrsBSuH_c.kz6hePyLmMwc" width="480" height="480"></iframe></center>
+                    -->
+                    <div id="venueMap"></div>
                     <!--
                     <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps?t=h&amp;sll=33.7748275,-84.2963123&amp;sspn=0.0016637,0.0026687&amp;q=Dekalb+History+Center+Museum&amp;cid=0x63f5425ba2131ec0&amp;ie=UTF8&amp;hq=Dekalb+History+Center+Museum&amp;hnear=&amp;ll=33.774828,-84.296312&amp;spn=0.006295,0.006295&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://www.google.com/maps?t=h&amp;sll=33.7748275,-84.2963123&amp;sspn=0.0016637,0.0026687&amp;q=Dekalb+History+Center+Museum&amp;cid=0x63f5425ba2131ec0&amp;ie=UTF8&amp;hq=Dekalb+History+Center+Museum&amp;hnear=&amp;ll=33.774828,-84.296312&amp;spn=0.006295,0.006295&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
                     -->
@@ -137,14 +139,15 @@
             </div>
             <div class="row">
                 <div class="col-sm-6">
-                    <h4>Courtyard Marriot</h4>
-
+                    <h4 class="center">Courtyard Marriot</h4>
+                    <div id="courtyardMap"></div>
                     <!--
                     <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=courtyard+marriott+near+Decatur,+GA&amp;aq=0&amp;oq=courtyard+marriott+de&amp;sll=33.775202,-84.296317&amp;sspn=0.001958,0.00284&amp;t=h&amp;ie=UTF8&amp;hq=courtyard+marriott&amp;hnear=Decatur,+DeKalb,+Georgia&amp;fll=33.776704,-84.296583&amp;fspn=0.002135,0.00284&amp;st=101124652325230410087&amp;rq=1&amp;ev=zi&amp;split=1&amp;ll=33.861906,-84.265015&amp;spn=0.002135,0.00284&amp;output=embed"></iframe><br /><small><a href="https://www.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=courtyard+marriott+near+Decatur,+GA&amp;aq=0&amp;oq=courtyard+marriott+de&amp;sll=33.775202,-84.296317&amp;sspn=0.001958,0.00284&amp;t=h&amp;ie=UTF8&amp;hq=courtyard+marriott&amp;hnear=Decatur,+DeKalb,+Georgia&amp;fll=33.776704,-84.296583&amp;fspn=0.002135,0.00284&amp;st=101124652325230410087&amp;rq=1&amp;ev=zi&amp;split=1&amp;ll=33.861906,-84.265015&amp;spn=0.002135,0.00284" style="color:#0000FF;text-align:left">View Larger Map</a></small>
                     -->
                 </div>
                 <div class="col-sm-6">
-                    <h4>W Midtown</h4>
+                    <h4 class="center">W Midtown</h4>
+                    <div id="wMap"></div>
                     <!--
                     <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=W+Atlanta+-+Midtown,+14th+Street+Northeast,+Atlanta,+GA&amp;aq=0&amp;oq=w+midtown&amp;sll=33.776704,-84.296583&amp;sspn=0.002135,0.00284&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=33.786952,-84.381661&amp;spn=0.006295,0.006295&amp;output=embed"></iframe><br /><small><a href="https://www.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=W+Atlanta+-+Midtown,+14th+Street+Northeast,+Atlanta,+GA&amp;aq=0&amp;oq=w+midtown&amp;sll=33.776704,-84.296583&amp;sspn=0.002135,0.00284&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;ll=33.786952,-84.381661&amp;spn=0.006295,0.006295" style="color:#0000FF;text-align:left">View Larger Map</a></small>
                     -->
@@ -164,13 +167,13 @@
             <h2 class="center header">Registry</h2>
             <div class="row">
                 <div class="col-sm-4">
-                    <center><a href="http://www.crateandbarrel.com/Gift-Registry/Brittany-Baptiste-and-Sam-Reaves/r4619566"><img src="./images/crateAndBarrel.png" class="img-responsive"/></a></center>
+                    <center><a href="http://www.crateandbarrel.com/Gift-Registry/Brittany-Baptiste-and-Sam-Reaves/r4619566"><img src="./images/crateAndBarrel.png" class="img-circle img-responsive"/></a></center>
                 </div>
                 <div class="col-sm-4">
-                    <center><a href="http://www2.myregistry.com/Visitors/GiftList/?panel=GuestBook"><img src="./images/myRegistry.png" class="img-responsive"/></a></center>
+                    <center><a href="http://www2.myregistry.com/Visitors/GiftList/?panel=GuestBook"><img src="./images/myRegistry.png" class="img-circle img-responsive"/></a></center>
                 </div>
                 <div class="col-sm-4">
-                    <center><a href="http://www1.macys.com/registry/wedding/guest/?registryId=6090352"><img src="./images/macys.png" class="img-responsive"/></a></center>
+                    <center><a href="http://www1.macys.com/registry/wedding/guest/?registryId=6090352"><img src="./images/macys.png" class="img-circle img-responsive"/></a></center>
                 </div>
             </div>
         </div>
@@ -182,6 +185,30 @@
         <script src="./js/wedding.js"></script>
         <!--<script type="text/javascript" src="https://raw.github.com/tuupola/jquery_lazyload/master/jquery.lazyload.min.js"></script>-->
         <script src="./js/gallery/responsiveslides.min.js"></script>
-
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDaHr3QdelytOo6L2DV_R_XZ3zUicjnEL0&sensor=false">
+        </script>
+        <script type="text/javascript">
+            function initialize() {
+                var venueOptions = {
+                    center: new google.maps.LatLng(33.77493, -84.29699),
+                    zoom: 2
+                };
+                var venueMap = new google.maps.Map(document.getElementById("venueMap"),
+                        venueOptions);
+                var wOptions = {
+                    center: new google.maps.LatLng(-34.397, 150.644),
+                    zoom: 8
+                };
+                var wMap = new google.maps.Map(document.getElementById("wMap"),
+                        wOptions);
+                var courtyardOptions = {
+                    center: new google.maps.LatLng(-34.397, 150.644),
+                    zoom: 8
+                };
+                var courtyardMap = new google.maps.Map(document.getElementById("courtyardMap"),
+                        courtyardOptions);
+            }
+            google.maps.event.addDomListener(window, 'load', initialize);
+        </script>
     </body>
 </html>
