ffb06dd417f1dc20fee8e2856ccbd8b7ee73fa04
Sam Reaves <thesamreaves@gmail.com>
2014-03-23 13:01:27 -0400

    Changing slideshow to more traditional style. Fixed width and selectors

--- js/wedding.js
+++ js/wedding.js
@@ -132,7 +132,14 @@ $(function() {
     };
     // Initialize responsive slides
     $(".rslides").responsiveSlides({
-        auto: true, // Boolean: Animate automatically, true or false
+        auto: false,
+        pager: true,
+        nav: true,
+        speed: 500,
+        maxwidth: 800,
+        namespace: "centered-btns"
+        
+        /*auto: true, // Boolean: Animate automatically, true or false
         speed: 500, // Integer: Speed of the transition, in milliseconds
         timeout: 4000, // Integer: Time between slide transitions, in milliseconds
         pager: true, // Boolean: Show pager, true or false
@@ -143,14 +150,14 @@ $(function() {
         prevText: "Previous", // String: Text for the "previous" button
         nextText: "Next", // String: Text for the "next" button
         maxwidth: "", // Integer: Max-width of the slideshow, in pixels
-        maxheight: "600",
+        maxheight: "300",
         navContainer: "", // Selector: Where controls should be appended to, default is after the 'ul'
         manualControls: "", // Selector: Declare custom pager navigation
         namespace: "rslides", // String: Change the default namespace used
         before: function() {
         }, // Function: Before callback
         after: function() {
-        }     // Function: After callback
+        }   */  // Function: After callback
     });
     ////////////////////////////////////////////////////
     // Change active tab when user scrolls to section //
